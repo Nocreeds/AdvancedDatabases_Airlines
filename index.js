23498535e1072d8f3bfc5a38b885436476e00a5d
Nishchhal <nishchhalbakshi@gmail.com>
2022-06-02 23:55:43 +0200

    test to move plane on leaflet

--- index.js
+++ index.js
@@ -5,5 +5,30 @@ var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+
+var planeIcon = L.icon({
+    iconUrl: 'plane.png',
+    shadowUrl: 'plane_shadow.png',
+
+    iconSize:     [30, 30], // size of the icon
+    shadowSize:   [35, 35], // size of the shadow
+    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+    shadowAnchor: [12, 5],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+var marker = L.marker([51.5, -0.09], {icon: planeIcon}).addTo(map);
+
+// setTimeout(() => {
+//     console.log("Hello");
+//     var latlng = L.latLng(51.501, -0.09);
+//     marker.setLatLng(latlng);
+// }, 2000)
+
+
+// setInterval(() => {
+//     var latlng = marker.getLatLng();
+//     latlng.lat += 0.0001;
+//     latlng.lng += 0.0001;
+//     marker.setLatLng(latlng);
+// }, 16);
 
